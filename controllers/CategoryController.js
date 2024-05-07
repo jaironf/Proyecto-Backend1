684036c251cb5d21280618696d2d089a4a4c1149
jaironf <jaironf.94@gmail.com>
2024-05-07 14:02:04 +0200

    Añadido relacion order-product

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,7 +4,8 @@ const CategoryController = {
     async create(req, res){
         try {
             const category = await Category.create(req.body)
-            res.status(201).send({msg: 'Categoria creada con éxito'})
+            res.status(201).send({msg: 'Categoria creada con éxito', category}) 
+        
         } catch (error) {
             console.error(error);
             res.status(500).send(error)
--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -4,6 +4,7 @@ const OrderController = {
     async create(req,res){
         try {
             const order = await Order.create(req.body)
+            order.addProduct(req.body.ProductId)
             res.status(201).send({msg: 'Pedido creado con exito', order})
         } catch (error) {
             console.error(error);
@@ -13,7 +14,7 @@ const OrderController = {
     async getAll(req,res){
         try {
             const orders = await Order.findAll({
-                include:[{model: Product, attributes:['name','price','genre'], through: { attributes: [] }}],
+                include:[{model: Product, attributes:['id','name','price'], through: { attributes: [] }}],
             });
             res.send(orders)
         } catch (error) {
